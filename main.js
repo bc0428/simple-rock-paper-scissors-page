45b114de017e9b9548c42275ea17256d31dbf3ce
bc0428 <briancheng28@yahoo.com.hk>
2022-12-06 22:33:55 +0800

    Added button and result div to the body

--- main.js
+++ main.js
@@ -1,5 +1,33 @@
-function hello(){
-    console.log('hello')
+function getComputerChoice(){
+    choice = ['rock', 'paper', 'scissors']
+    return choice[Math.floor(Math.random() * 3)]
 }
 
-hello()
+function playRound(playerSelection, computerSelection){
+    playerSelection = String(playerSelection).toLowerCase()
+
+    if ((playerSelection == 'rock' && computerSelection == 'scissors') || 
+    (playerSelection == 'paper' && computerSelection == 'rock') || 
+    (playerSelection == 'scissors' && computerSelection == 'paper')){
+        console.log(playerSelection + 'beats' + computerSelection + '. You Win!')
+    }
+    else if ((playerSelection == 'rock' && computerSelection == 'paper') || 
+    (playerSelection == 'paper' && computerSelection == 'scissors') || 
+    (playerSelection == 'scissors' && computerSelection == 'rock')){
+        console.log(computerSelection + 'beats' + playerSelection + '. You Lose!')
+    }
+    else if (playerSelection == computerSelection){
+        console.log(playerSelection + 'and' + computerSelection + '. Draw!')
+    }
+    else{
+        console.log('ERROR')
+    }
+}
+
+function game(){
+    for (let i =0; i<5; i++){
+        let playerSelection = window.prompt("Your choice")
+        playRound(playerSelection, getComputerChoice)
+    }
+}
+game()
