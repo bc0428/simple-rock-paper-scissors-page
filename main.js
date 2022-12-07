c2fa2bdfd8cc40462cc6d025fc97c268bc7adb48
bc0428 <briancheng28@yahoo.com.hk>
2022-12-07 22:42:49 +0800

    Added transition effect for buttons

--- main.js
+++ main.js
@@ -1,33 +1,12 @@
-function getComputerChoice(){
-    choice = ['rock', 'paper', 'scissors']
-    return choice[Math.floor(Math.random() * 3)]
-}
+const buttons = document.querySelectorAll('button')
+const results = document.querySelector('.results')
 
-function playRound(playerSelection, computerSelection){
-    playerSelection = String(playerSelection).toLowerCase()
+buttons.forEach((button) => [
+    button.addEventListener('click', makeChoice)
 
-    if ((playerSelection == 'rock' && computerSelection == 'scissors') || 
-    (playerSelection == 'paper' && computerSelection == 'rock') || 
-    (playerSelection == 'scissors' && computerSelection == 'paper')){
-        console.log(playerSelection + 'beats' + computerSelection + '. You Win!')
-    }
-    else if ((playerSelection == 'rock' && computerSelection == 'paper') || 
-    (playerSelection == 'paper' && computerSelection == 'scissors') || 
-    (playerSelection == 'scissors' && computerSelection == 'rock')){
-        console.log(computerSelection + 'beats' + playerSelection + '. You Lose!')
-    }
-    else if (playerSelection == computerSelection){
-        console.log(playerSelection + 'and' + computerSelection + '. Draw!')
-    }
-    else{
-        console.log('ERROR')
-    }
-}
+])
 
-function game(){
-    for (let i =0; i<5; i++){
-        let playerSelection = window.prompt("Your choice")
-        playRound(playerSelection, getComputerChoice)
-    }
+
+function makeChoice(e) {
+    results.textContent = String(e.target.className)
 }
-game()
